d9901672a41c204309d23f389fbe8f6599f190d9
Steven Schrembeck <steven.schrembeck@gmail.com>
2021-08-09 12:38:26 -0400

    Debugging

--- src/actions/facebookcustomermatch/lib/executor.ts
+++ src/actions/facebookcustomermatch/lib/executor.ts
@@ -195,7 +195,7 @@ export default class FacebookCustomerMatchExecutor {
   private async startAsyncParser(downloadStream: Readable) {
     return new Promise<void>((resolve, reject) => {
       oboe(downloadStream)
-        .node("*", (row: any, path: string, ancestors: any[]) => {    
+        .node("!.*", (row: any, path: string, ancestors: any[]) => {    
           debugger;      
           console.log(path)
           console.log(ancestors)
