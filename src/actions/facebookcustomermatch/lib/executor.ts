a3eb09485225ab7a95f5cce1ba854cbe7153a75e
Steven Schrembeck <steven.schrembeck@gmail.com>
2021-08-09 11:59:34 -0400

    Debugging

--- src/actions/facebookcustomermatch/lib/executor.ts
+++ src/actions/facebookcustomermatch/lib/executor.ts
@@ -195,7 +195,8 @@ export default class FacebookCustomerMatchExecutor {
   private async startAsyncParser(downloadStream: Readable) {
     return new Promise<void>((resolve, reject) => {
       oboe(downloadStream)
-        .node("!.*", (row: any) => {          
+        .node("!.*", (row: any) => {    
+          debugger;      
           if (!this.isSchemaDetermined) {
             this.determineSchema(row)
           }
